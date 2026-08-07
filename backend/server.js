c1c89275caaada67b90cd3b1508e5e18bf68109c
Priyanshu <mimoohk23@gmail.com>
2026-08-07 23:11:41 +0530

    initial backend commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB  =require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at port ${PORT}`);
+});
