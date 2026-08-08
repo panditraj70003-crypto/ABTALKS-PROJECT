c777261b120096871eae3fdbadffa384e27c9965
Raj Pandit <panditraj70003@gmail.com>
2026-08-08 21:32:11 +0530

    POST WORK

--- backend/modules/post/post.service.js
+++ backend/modules/post/post.service.js
@@ -1,17 +1,14 @@
 const Post = require("./post.model");
 
-
 const createPost = async (data) => {
     return await Post.create(data);
 };
-
 
 const getAgentPosts = async (agentId) => {
     return await Post.find({ agentId })
         .sort({ createdAt: -1 })
         .lean();
 };
-
 
 module.exports = {
     createPost,
